1e07afe31b9258fe1e30340f90feaa559fc49933
默默之 <36978218+xw-momozhi@users.noreply.github.com>
2019-05-05 14:37:46 +0800

    Update message.js

--- message.js
+++ message.js
@@ -1,4 +1,109 @@
 var packing=(function(){
+	var m_document_addEventListener = document.addEventListener;
+	var m_document_removeEventListener = document.removeEventListener;
+	var m_window_addEventListener = window.addEventListener;
+	var m_window_removeEventListener = window.removeEventListener;
+	var documentEventHandlers = {},//事件订阅
+    windowEventHandlers = {};//事件订阅
+	document.addEventListener=function(evt, handler, capture){
+		var e = evt.toLowerCase();
+		if (typeof documentEventHandlers[e] != 'undefined') {
+			documentEventHandlers[e].subscribe(handler);
+		} else {
+			m_document_addEventListener.call(document, evt, handler, capture);
+		}
+	}
+	window.addEventListener=function(evt, handler, capture){
+		var e = evt.toLowerCase();
+		if (typeof windowEventHandlers[e] != 'undefined') {
+			windowEventHandlers[e].subscribe(handler);
+		} else {
+			m_document_addEventListener.call(window, evt, handler, capture);
+		}
+	}
+	document.removeEventListener = function(evt, handler, capture) {
+		var e = evt.toLowerCase();
+		if (typeof documentEventHandlers[e] != "undefined") {
+			documentEventHandlers[e].unsubscribe(handler);
+		} else {
+			m_document_removeEventListener.call(document, evt, handler, capture);
+		}
+	};
+
+	window.removeEventListener = function(evt, handler, capture) {
+		var e = evt.toLowerCase();
+		if (typeof windowEventHandlers[e] != "undefined") {
+			windowEventHandlers[e].unsubscribe(handler);
+		} else {
+			m_window_removeEventListener.call(window, evt, handler, capture);
+		}
+	};
+	
+	//触发事件
+	function evenTrigger(evt,data){
+		var evenName = evt.toLowerCase();
+		if (typeof windowEventHandlers[evenName] != 'undefined') {
+			windowEventHandlers[evenName].trigger(data);
+		}else if (typeof documentEventHandlers[evenName] != 'undefined') {
+			documentEventHandlers[evenName].trigger(data);
+		}else {
+			var myEvent = new CustomEvent(evenName,{detail:data});
+			// 随后在对应的元素上触发该事件
+			if(window.dispatchEvent) {  
+				document.dispatchEvent(myEvent);
+			} else {
+				document.dispatchEvent(myEvent);
+			}
+		}
+	}
+	//添加自定义事件
+	function addEvent(evenName,handler){
+		if (typeof documentEventHandlers[evenName] != "undefined") {
+			documentEventHandlers[evenName]=new PubSub()
+		}
+		window.addEventListener(evenName, handler,false);
+		pushEven[evenName]=handler;
+	}
+	//移除自定义事件
+	function removeEvent(evenName,handler){
+		window.removeEventListener(evenName,handler,false);
+	    delete pushEven[evenName];
+	}
+	function PubSub() {
+	  this.handlers = [];
+	}
+	PubSub.prototype = {
+		// 订阅事件
+		subscribe: function(handler){
+			this.handlers.push(handler);
+			return this;
+		},
+		 // 触发事件(发布事件)
+		trigger: function(data){
+		   var self = this;
+		   for(var i = 0; i < self.handlers.length; i++) {
+			 self.handlers[i].apply(self,[data]);
+		   }
+		   return self;
+		},
+		// 删除订阅事件
+		unsubscribe: function(eventType, handler){
+			var currentEvent = this.handlers;
+			var len = 0;
+			if (currentEvent) {
+				len = currentEvent.length;
+				for (var i = len - 1; i >= 0; i--){
+					if (currentEvent[i] === handler){
+						currentEvent.splice(i, 1);
+					}
+				}
+			}
+			return this;
+		}
+	};
+	documentEventHandlers["deviceready"]=new PubSub();
+	/**以上是对事件的处理**/
+	
 	var evenID=0;
 	var delEven={};
 	var pushEven={};
@@ -100,38 +205,23 @@ var packing=(function(){
 			evenTrigger(data.name,data.data);
 		}
 	}, false);
-	//触发事件
-	function evenTrigger(evenName,data){
-		var myEvent = new CustomEvent(evenName,{detail:data});
-			// 随后在对应的元素上触发该事件
-		if(window.dispatchEvent) {  
-			window.dispatchEvent(myEvent) || document.dispatchEvent(myEvent);
-		} else {
-			document.fireEvent(myEvent) || document.dispatchEvent(myEvent);
-		}
-	}
-	//添加自定义事件
-	function addEvent(evenName,handler){
-		window.addEventListener(evenName, handler,false);
-		pushEven[evenName]=handler;
-	}
-	//移除自定义事件
-	function removeEvent(evenName,handler){
-		window.removeEventListener(evenName,handler,false);
-	    delete pushEven[evenName];
-	}
+	
 	return {
 		setStringJson:stringJson,
 		sendMessage:function(name,json){//发送消息
 			//向子窗口发送消息
-			window.frames[0].postMessage({name:name,data:stringJson(json)}, '*');
+			//window.frames[0].postMessage({name:name,data:stringJson(json)}, '*');
 			//向父窗口发送消息
-			//window.parent.postMessage({name:name,data:stringJson(json)}, '*');
+			window.parent.postMessage({name:name,data:stringJson(json)}, '*');
 		},
 		evenfilter:function(callback){//过滤注册事件参数
 			return function(e){
 				if(e && e.detail){
 					var detail=e.detail;
+					if(callback){
+						callback(detail);
+					}
+				}else{
 					if(callback){
 						callback(detail);
 					}
